7011de68f7cce12b46963d770e68989dbecc54bd
Roberto.Latonda <robertogogo12@gmail.com>
2024-04-08 18:22:13 +0200

    prueba1

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
-prueba de subida
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+output "primary_access_key" {
+  value = azurerm_storage_account.example.primary_access_key
+}
